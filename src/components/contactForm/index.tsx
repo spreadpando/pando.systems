8596e05cc52b59adda9718d8c8b6c00b668a2c05
pando <spreadpando@gmail.com>
2021-06-04 16:09:57 -0600

    updated /api/contact.js

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -3,120 +3,124 @@ import { useForm } from 'react-hook-form'
 import styled from '@emotion/styled'
 
 const FlexContainer = styled('div')`
-  position: absolute;
-  display: flex;
-  z-index: 0;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
+	position: absolute;
+	display: flex;
+	z-index: 0;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
 `
 
 const Container = styled('div')`
-  position: absolute;
-  width: 14rem;
-  text-align: right;
-  z-index: 2;
-  font-family: helvetica;
-  div {
-    position: relative;
-    width: 100%;
-  }
-  input {
-    background-color: transparent;
-    width: 100%;
-    margin: 1em 0;
-    border-width: 0 0 2px 0;
-    border-color: #000;
-    &:focus{
-      outline: none;
-    };
-  }
-  input[type='submit']{
-    width: 33%;
-    align-self: center;
-    margin: 2em;
-    border-width:  2px ;
-
-  }
-  textarea {
-    display: block;
-    width: 100%;
-    border-width: 2px;
-    border-color: #000;
-    border-radius: 3px;
-    background-color: transparent;
-    resize: none;
-    &:focus{
-      outline: none;
-    }
-  }
-  form {
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    width: 100%;
-  }
+	position: absolute;
+	width: 14rem;
+	text-align: right;
+	z-index: 2;
+	font-family: helvetica;
+	div {
+		position: relative;
+		width: 100%;
+	}
+	input {
+		background-color: transparent;
+		width: 100%;
+		margin: 1em 0;
+		border-width: 0 0 2px 0;
+		border-color: #000;
+		&:focus {
+			outline: none;
+		}
+	}
+	input[type='submit'] {
+		width: 33%;
+		align-self: center;
+		margin: 2em;
+		border-width: 2px;
+	}
+	textarea {
+		display: block;
+		width: 100%;
+		border-width: 2px;
+		border-color: #000;
+		border-radius: 3px;
+		background-color: transparent;
+		resize: none;
+		&:focus {
+			outline: none;
+		}
+	}
+	form {
+		position: relative;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		width: 100%;
+	}
 `
 interface Inputs {
-  name: string
-  email: string
-  message: string
+	name: string
+	email: string
+	message: string
 }
 const Contact: React.FC = () => {
-  const [submitted, setSubmitted] = useState(false)
-  const { register, handleSubmit } = useForm<Inputs>()
-  const onSubmit = async (data: Inputs): Promise<void> => {
-    console.log('submitted')
-    const res = await fetch('/api/contact', { // eslint-disable-line
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-    const text = await res.text()
-    if (text === 'success') {
-      setSubmitted(true)
-    }
-  }
+	const [submitted, setSubmitted] = useState(false)
+	const { register, handleSubmit } = useForm<Inputs>()
+	const onSubmit = async (data: Inputs): Promise<void> => {
+		console.log('submitted')
+		const res = await fetch('/api/contact', {
+			// eslint-disable-line
+			method: 'POST',
+			body: JSON.stringify(data)
+		})
+		const text = await res.text()
+		if (text === 'success') {
+			setSubmitted(true)
+		}
+	}
 
-  return (
-    <FlexContainer>
-      <Container>
-          {submitted
-            ? (
-              <h2 key='head'>sent.</h2>
-              )
-            : (
-              <form
-                key='form'
-                onSubmit={handleSubmit(() => onSubmit)}
-              >
-                <div>
-                  <label>
-              name
-                  </label>
-                  <input type='text' id='name' name='name' {...register('name', { required: true })} />
-                </div>
-                <div>
-                  <label>
-              email
-                  </label>
-                  <input type='email' id='email' name='email' {...register('email', { required: true })} />
-                </div>
-                <div>
-                  <label>
-              message
-                  </label>
-                  <textarea rows={4} id='comment' name='comment' {...register('message')} />
-                </div>
-                <input type='submit' value='send' />
-              </form>
-              )}
-      </Container>
-    </FlexContainer>
-  )
+	return (
+		<FlexContainer>
+			<Container>
+				{submitted ? (
+					<h2 key="head">sent.</h2>
+				) : (
+					<form key="form" onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<label>name</label>
+							<input
+								type="text"
+								id="name"
+								name="name"
+								{...register('name', { required: true })}
+							/>
+						</div>
+						<div>
+							<label>email</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								{...register('email', { required: true })}
+							/>
+						</div>
+						<div>
+							<label>message</label>
+							<textarea
+								rows={4}
+								id="comment"
+								name="comment"
+								{...register('message')}
+							/>
+						</div>
+						<input type="submit" value="send" />
+					</form>
+				)}
+			</Container>
+		</FlexContainer>
+	)
 }
 
 export default Contact
--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -1,34 +1,40 @@
 import sgMail from '@sendgrid/mail'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  return await new Promise(resolve => {
-    const { name, email, comment } = JSON.parse(req.body)
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-      to: 'chad.paley.henderson@gmail.com',
-      from: 'no-reply@chaderson.com',
-      replyTo: `${email}`,
-      subject: `${name} via chaderson.com`,
-      text: `comment: ${comment}`
-    }
-    sgMail.send(msg).then(() => {
-      res.status(200).send('success')
-      return resolve()
-    }).catch(err => {
-      console.log(err)
-      if (err.response != null) {
-      // Extract error msg
-        const { message, code, response } = err
+export default async (
+	req: NextApiRequest,
+	res: NextApiResponse
+): Promise<void> => {
+	return await new Promise(resolve => {
+		const { name, email, comment } = JSON.parse(req.body)
+		sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+		const msg = {
+			to: 'spreadpando@gmail.com',
+			from: 'no-reply@pando.systems',
+			replyTo: `${email}`,
+			subject: `${name} via pando.systems`,
+			text: `comment: ${comment}`
+		}
+		sgMail
+			.send(msg)
+			.then(() => {
+				res.status(200).send('success')
+				return resolve()
+			})
+			.catch(err => {
+				console.log(err)
+				if (err.response != null) {
+					// Extract error msg
+					const { message, code, response } = err
 
-        // Extract response msg
-        const { headers, body } = response
+					// Extract response msg
+					const { headers, body } = response
 
-        console.error(body)
-        console.error(message, code, headers)
-        res.status(500).send('failure')
-        return resolve()
-      }
-    })
-  })
+					console.error(body)
+					console.error(message, code, headers)
+					res.status(500).send('failure')
+					return resolve()
+				}
+			})
+	})
 }
